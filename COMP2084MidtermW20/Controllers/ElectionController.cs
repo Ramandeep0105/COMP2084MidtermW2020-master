538ac1b522ba897ca3ba1c25d647bee2957a27ae
ramandeep0105 <ramanbalu902@gmail.com>
2020-02-20 20:55:00 -0500

    Add project files.

---
+++ COMP2084MidtermW20/Controllers/ElectionController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP2084MidtermW20.Controllers
+{
+    public class ElectionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ COMP2084MidtermW20/Models/Election.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP2084MidtermW20.Models
+{
+    public class Election
+    {
+        public int ElectionId { get; set; }
+        public int Year { get; set; }
+        public int PartyId { get; set; }
+        public bool? Majority { get; set; }
+
+        [ForeignKey("PartyId")]
+        [InverseProperty("Election")]
+        public virtual Party Party { get; set; }
+    }
+}
